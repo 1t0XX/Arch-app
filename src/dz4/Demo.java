5a794172bf9f54a796a556b29d57ff2d753bf7fa
1t0XX <53831070+1t0XX@users.noreply.github.com>
2021-01-04 18:24:26 +0300

    dz4

---
+++ src/dz4/Demo.java
@@ -0,0 +1,27 @@
+package dz4;
+
+import dz4.devices.Device;
+import dz4.devices.Radio;
+import dz4.devices.Tv;
+import dz4.remotes.AdvancedRemote;
+import dz4.remotes.BasicRemote;
+
+public class Demo {
+    public static void main(String[] args) {
+        testDevice(new Tv());
+        testDevice(new Radio());
+    }
+
+    public static void testDevice(Device device) {
+        System.out.println("Tests with basic remote.");
+        BasicRemote basicRemote = new BasicRemote(device);
+        basicRemote.power();
+        device.printStatus();
+
+        System.out.println("Tests with advanced remote.");
+        AdvancedRemote advancedRemote = new AdvancedRemote(device);
+        advancedRemote.power();
+        advancedRemote.mute();
+        device.printStatus();
+    }
+}
